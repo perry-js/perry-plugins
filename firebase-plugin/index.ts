171f837743661e3be9fa6541017c4518baa6801e
Guilherme Lima <guilhermelimak@gmail.com>
2018-10-19 00:14:00 -0300

    fix: Add default options to firebase plugin config

--- firebase-plugin/index.ts
+++ firebase-plugin/index.ts
@@ -1,9 +1,19 @@
-import firebase from "firebase";
 import { IFirebasePluginConfig } from "./interfaces/firebase-plugin-config";
 import { writeToDB } from "./write-to-db";
 
-export default (cfg: IFirebasePluginConfig) => (report: PerryReport) => {
-  const basePath = cfg.basePath ? cfg.basePath : "perry_reports";
-  const ref = cfg.firebaseApp.database().ref(`${basePath}/${report.id}`);
-  writeToDB(ref, cfg.transform ? cfg.transform(report) : report);
+const defaultConfig = {
+  basePath: 'perry_reports',
+  transform: (report: PerryReport) => report,
+}
+
+export const FirebasePlugin = (userConfig: IFirebasePluginConfig) => (report: PerryReport) => {
+  const config: IFirebasePluginConfig = {
+    ...defaultConfig,
+    ...userConfig
+  }
+
+  const basePath = config.basePath;
+  const ref = config.firebaseApp.database().ref(`${basePath}/${report.id}`);
+
+  writeToDB(ref, config.transform(report));
 };
